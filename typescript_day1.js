95e0a9e8b51fba956592edd0fe79cda4d66ee1a8
alfauzan003 <habibalfauzan1@gmail.com>
2022-10-28 15:13:43 +0700

    .

--- typescript_day1.js
+++
@@ -1,6 +0,0 @@
-function slicer(text, start, end) {
-    var hasil;
-    hasil = text.slice(start, end);
-    return hasil;
-}
-console.log(slicer('Learning Typescript is different than Javascript', 9, 19));
--- typescript_day1.ts
+++
@@ -1,6 +0,0 @@
-function slicer(text:string, start:number, end:number): string{
-    let hasil:string;
-    hasil = text.slice(start, end);
-    return hasil;
-}
-console.log(slicer('Learning Typescript is different than Javascript', 9, 19));
