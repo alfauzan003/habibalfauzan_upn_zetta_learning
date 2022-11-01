77ef33ee2bd11f652fe75076051524dcdb191d53
alfauzan003 <habibalfauzan1@gmail.com>
2022-11-01 08:51:16 +0700

    Angular day 1

--- angular_learning/src/app/app.module.ts
+++ angular_learning/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Module1Component } from './module1/module1.component';
+import { Module2Component } from './module2/module2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Module1Component,
+    Module2Component
   ],
   imports: [
     BrowserModule,
