75bd16ab62ba9c5f75100df28b9a9bace08f2e28
alfauzan003 <habibalfauzan1@gmail.com>
2022-11-18 13:31:04 +0700

    Angular Day 11

--- angular_learning/src/app/app-routing.module.ts
+++ angular_learning/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { AddDataComponent } from './add/add-data/add-data.component';
+import { ListDataComponent } from './list/list-data/list-data.component';
+import { EditDataComponent } from './edit/edit-data/edit-data.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'edit/:id', component: EditDataComponent },
+  { path: '', component: ListDataComponent },
+  { path: 'add', component: AddDataComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
